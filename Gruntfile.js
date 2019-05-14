5e7f3e1226a06bcd64a0fef304052864911eed17
AndrewBellamy <andymanbellamy@gmail.com>
2019-05-14 17:28:17 +0930

    feature(deploy task): (#6)
    * feature(deploy task):
    
    	modified:   .travis.yml
    	modified:   Gruntfile.js
    	modified:   composer.json
    	modified:   package.json
    	modified:   yarn.lock
    
    * fix up env variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,27 @@ module.exports = function(grunt) {
                 src: '**',
                 dest: 'build/'
             }
+        },
+        sftp: {
+            deakin: {
+                files: {
+                    "./": ["build/api/**"]
+                },
+                options: {
+                    path: '/home/abellamy/public_html/SARMS',
+                    username: '<%= process.env.CD_USR %>',
+                    password: '<%= process.env.CD_PWD %>',
+                    host: 'ftp.deakin.edu.au',
+                    srcBasePath: 'build/',
+                    createDirectories: true,
+                    showProgress: true
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.registerTask('build', ['copy:build']);
+
+    grunt.loadNpmTasks('grunt-ssh');
+    grunt.registerTask('deploy', ['sftp:deakin']);
 };
